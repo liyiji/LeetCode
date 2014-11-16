5745625867c98058858e6f569d5c6e7cda44c9fe
liyiji <liyijipersonal@gmail.com>
2014-11-16 23:29:18 +0800

    Update 146_Min_Stack.cpp

--- 146_Min_Stack.cpp
+++ 146_Min_Stack.cpp
@@ -1,40 +1,42 @@
 /*
- * Min Stack Total Accepted: 704 Total Submissions: 4358 My Submissions Question Solution
+ * Min Stack
+ * Total Accepted: 4819 Total Submissions: 31323
+ *
  * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
  *
- * push(x) -- Push element x onto stack.
- * pop() -- Removes the element on top of the stack.
- * top() -- Get the top element.
- * getMin() -- Retrieve the minimum element in the stack.
+ *     push(x) -- Push element x onto stack.
+ *     pop() -- Removes the element on top of the stack.
+ *     top() -- Get the top element.
+ *     getMin() -- Retrieve the minimum element in the stack.
+ *
  */
 // Tips: Do NOT use std::vector. This will cause Memory Limit Exceeded. Use std::stack instead.
 class MinStack
 {
 public:
-	stack<int> datas, mins;
+    stack<int> datas, mins;
 
-	void push(int x)
-	{
-		datas.push(x);
-		if (mins.empty() || x <= mins.top())
-			mins.push(x);
-	}
+    void push(int x)
+    {
+        datas.push(x);
+        if (mins.empty() || x <= mins.top())
+            mins.push(x);
+    }
 
-	void pop()
-	{
-		if (mins.top() >= datas.top())
-			mins.pop();
-		datas.pop();
+    void pop()
+    {
+        if (mins.top() >= datas.top())
+            mins.pop();
+        datas.pop();
+    }
 
-	}
+    int top()
+    {
+        return datas.top();
+    }
 
-	int top()
-	{
-		return datas.top();
-	}
-
-	int getMin()
-	{
-		return mins.top();
-	}
+    int getMin()
+    {
+        return mins.top();
+    }
 };
